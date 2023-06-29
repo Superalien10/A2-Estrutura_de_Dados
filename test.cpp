118b468a78f05ec72935401bf772be2101830ed1
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2023-06-29 15:18:50 -0300

    Update test.cpp
    Crie teste para a função Readtxt

--- test.cpp
+++ test.cpp
@@ -3,5 +3,14 @@
 using namespace std;
 int main()
 {
+    cout << "Teste de funcoes" << endl;
+    string resultado;
+
+    cout << "Teste de Readtxt" << endl;
+    resultado=("1 5 7 18 2 3 "==Readtxt("arvore_1.txt"))? "BEM-SUCEDIDO":"FALHOU";
+    cout << Readtxt("arvore_1.txt") << endl;
+    cout << "Leitura arvore_1.txt: " << resultado << endl;
+    
+    cout << "Fim dos testes" << endl;
     return 0;
 }
