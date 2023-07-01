980d8b10240d3d7e2880df3496adeaec052bcbd7
Superalien10 <105555970+Superalien10@users.noreply.github.com>
2023-07-01 01:53:52 -0300

    Comente funcoesVisualizacao.cpp
    Explique melhor como funcionam as funções do arquivo

--- funcoesVisualizacao.cpp
+++ funcoesVisualizacao.cpp
@@ -7,13 +7,22 @@
 
 using namespace std;
 
+// Essas funções permitem a visualização do processo de ordenação dos elementos de uma árvore binária.
+// Com elas nós etiquetamos os nós para podermos representá-los graficamente, com base em sua posição.
 
-// Função que exibe uma representação gráfica de3 uma lista.
 void visualizeList(struct ListNode* npListNode, struct ListNode* npActive1, struct ListNode* npActive2)
 {
+    /* A função exibe uma representação gráfica de uma lista duplamente encadeada. Ela destaca os nós assinalados como ativos.
+
+    argumentos:
+        npListNode: ponteiro para o primeiro nó da lista
+        npActive1: ponteiro para o primeiro nó ativo (nó que está sendo trocado)
+        npActive2: ponteiro para o segundo nó ativo
+    */
     struct ListNode* npTemp = npListNode;
     if (npTemp == nullptr)
     {
+        // Caso o ponteiro para o primeiro nó seja nulo, a lista está vazia. Um aviso é dado ao usuário.
         cout << "Nao e possivel exibir: lista vazia" << endl;
         return;
     }
@@ -22,6 +31,7 @@ void visualizeList(struct ListNode* npListNode, struct ListNode* npActive1, stru
     {
         while (npTemp->npPrev!=nullptr)
         {
+            // O ponteiro é movido para o primeiro nó da lista.
             npTemp = npTemp->npPrev;
         }
     }
@@ -32,6 +42,7 @@ void visualizeList(struct ListNode* npListNode, struct ListNode* npActive1, stru
     {
         if (npTemp==npActive1||npTemp==npActive2)
         {
+            // Caso o nó seja ativo, ele é representado por um asterisco.
             cout << " | ";
             for (int i=0; i<npTemp->iPosition; i++)
             {
@@ -40,6 +51,7 @@ void visualizeList(struct ListNode* npListNode, struct ListNode* npActive1, stru
         }
         else
         {
+            // Caso contrário, ele é representado por um traço.
             cout << " | ";
             for (int i=0; i<npTemp->iPosition; i++)
             {
@@ -47,44 +59,48 @@ void visualizeList(struct ListNode* npListNode, struct ListNode* npActive1, stru
             }
         }
         cout << endl;
+        // O ponteiro é movido para o próximo nó.
         npTemp = npTemp->npNext;
     }
+    // A função sleep() é usada para dar um intervalo de tempo entre a exibição de cada etapa.
     sleep(1);
 }
 
-// Função que atribui posições aos nós de uma lista.
 void setPositions(struct ListNode* npListNode, struct ListNode* npOrdered)
 {
+    /* Essa função atribui posições aos nós de uma lista duplamente encadeada, com base na versão ordenada da mesma. Ela é usada para a visualização do processo de ordenação.
+    
+    argumentos:
+        npListNode: ponteiro para o primeiro nó da lista
+        npOrdered: ponteiro para o primeiro nó da lista ordenada
+    */
     struct ListNode* npTemp = npListNode;
     struct ListNode* npTemp2 = npOrdered;
-    // displayList(npListNode);
-    // displayList(npOrdered);
     int iPosition = 1;
     int iLast;
     while (npTemp2!=nullptr)
     {
         while (npTemp!=nullptr)
         {
+            // A função percorre a lista original e atribui posições aos nós que possuem o mesmo valor que o nó da lista ordenada que está sendo analisado.
             if (npTemp->iPayload==npTemp2->iPayload)
             {
                 npTemp->iPosition = iPosition;
-                // cout << "Check 1: " << iPosition << endl;
-                // cout << "Check 2: " << npTemp->iPosition << endl;
-                // cout << "Check 3: " << npTemp2->iPayload << endl;
-                // cout << "Check 4: " << npTemp->iPayload << endl;
                 npTemp = npTemp->npNext;
             }
             else
             {
                 npTemp = npTemp->npNext;
             }
+            // O ponteiro é movido para o próximo nó.
         }
+        // O ponteiro é movido para o primeiro nó da lista original.
         npTemp = npListNode;
         iLast=npTemp2->iPayload;
         bool bCondition = true;
         while (bCondition)
         {
-            // cout<<"Check 5: "<<npTemp2->iPayload<<endl;
+            // A função percorre a lista ordenada até encontrar um nó com valor diferente do nó que já foi analisado.
             if (npTemp2->npNext!=nullptr)
             {
                 npTemp2 = npTemp2->npNext;
@@ -99,8 +115,8 @@ void setPositions(struct ListNode* npListNode, struct ListNode* npOrdered)
                 npTemp2=nullptr;
             }
         }
+        // A posição é incrementada. De posse do próximo nó da lista ordenada, o processo é repetido.
         iPosition++;
     }
-    displayList(npListNode);
 }
 
